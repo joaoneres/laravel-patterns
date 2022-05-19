b5e888b3c15ed9293965f820af0fb8a64d08277f
João Vitor <leof.neres@gmail.com>
2022-05-19 13:22:10 -0300

    app service provider updates to consider car repository interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\CarRepositoryInterface;
+use App\Repositories\Eloquent\CarRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(CarRepositoryInterface::class, CarRepository::class);
     }
 
     /**
