3888437f8adbc6ff47ff086f9475ae96146e42fc
João Vitor <leof.neres@gmail.com>
2022-05-19 13:21:01 -0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/cars')->group(function () {
+    Route::get('/', [App\Http\Controllers\CarController::class, 'index'])->name('cars.index');
+    Route::get('/create', [App\Http\Controllers\CarController::class, 'create'])->name('cars.create');
+    Route::post('/store', [App\Http\Controllers\CarController::class, 'store'])->name('cars.store');
+    Route::get('/{id}/show', [App\Http\Controllers\CarController::class, 'show'])->name('cars.show');
+    Route::get('/{id}/edit', [App\Http\Controllers\CarController::class, 'edit'])->name('cars.edit');
+    Route::put('/{id}/update', [App\Http\Controllers\CarController::class, 'update'])->name('cars.update');
+    Route::delete('/{id}/delete', [App\Http\Controllers\CarController::class, 'delete'])->name('cars.delete');
+});
